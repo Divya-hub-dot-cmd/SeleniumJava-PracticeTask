19a490f17e1f35d0ea70433851d2eee554f42a68
Divya-hub-dot-cmd <dhivyavelumani003@gmail.com>
2025-12-09 22:30:30 +0530

    Check Whether the Number is Positive

---
+++ CheckNumberPositive.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class CheckNumberPositive {
+
+    public static void main(String[] args) {
+
+        int number = -5; 
+
+        if (number > 0) {
+            System.out.println(number + " is Positive");
+        } else if (number < 0) {
+            System.out.println(number + " is Negative");
+        } else {
+            System.out.println("The number is Zero");
+        }
+    }
+}
