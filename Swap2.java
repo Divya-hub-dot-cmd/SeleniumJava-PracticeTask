7e760bdfedf2fa755bd97bdf18932635e7879d26
Divya-hub-dot-cmd <dhivyavelumani003@gmail.com>
2026-01-05 23:00:57 +0530

    Swap2

---
+++ Swap2.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Swap2 {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		System.out.println("Before Swapping, a= "+a + " and b= " +b);
+		a=a^b;
+		b=a^b;
+		a=a^b;
+		System.out.println("After Swapping, a= "+a + " and b= " +b);
+
+	}
+
+}
