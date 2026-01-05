b1f19959161e84ffb0ff490466fb0d8146655d1a
Divya-hub-dot-cmd <dhivyavelumani003@gmail.com>
2026-01-05 23:00:27 +0530

    Swap1

---
+++ SwapUsingTemporaryVariable.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class SwapUsingTemporaryVariable {
+
+	public static void main(String[] args) {
+		
+		int x= 10, y=20;
+		System.out.println("Before Swap");
+		System.out.println("x= " + x);
+		System.out.println("y= " + y);
+		int temp = x;
+		x=y;
+		y=temp;
+		System.out.println("After Swap");
+		System.out.println("x= " +x);
+		System.out.println("y= " +y);
+
+	}
+
+}
